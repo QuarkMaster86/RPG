e0256ecded1066694d3c52adadd763b54eb007ee
QuarkMaster86 <seane443@gmail.com>
2024-11-05 12:02:19 -0800

    Third commit

--- obj/Debug/net8.0-windows/RPG.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/RPG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cffd3b86554d0293edd90a6a4e1d3411daaf91e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3b87ed6039575956f7d91def588fa9115a83cf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
